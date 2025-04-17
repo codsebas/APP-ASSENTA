6351d1c887a4814186b28ea6b690176048487730
javie <javier.fajardo098@gmail.com>
2025-04-17 12:09:35 -0600

    modificacion y creacion de implementacion metodos mostrar e insertar empleados

--- src/main/java/com/umg/implementacion/EmpleadoImp.java
+++ src/main/java/com/umg/implementacion/EmpleadoImp.java
@@ -20,6 +20,40 @@ public class EmpleadoImp implements IEmpleados {
 
     @Override
     public boolean insertarEmpleado(ModeloEmpleado modelo) {
+        boolean resultado = true;
+        conector.conectar();
+
+        try {
+            ps = conector.preparar(sql.getINSERTAR_EMPLEADO());
+            ps.setInt(1, modelo.getId_empleado());
+            ps.setString(2, modelo.getPrimerNombre());
+            ps.setString(3, modelo.getSegundoNombre());
+            ps.setString (4, modelo.getPrimerApellido());
+            ps.setString (5, modelo.getSegundoApellido());
+            ps.setString (6, modelo.getFechaNacimiento());
+            ps.setInt(7, modelo.getEdad());
+            ps.setString(8, modelo.getDpi());
+            ps.setString(10, modelo.getNumeroTelefono());
+            ps.setString(11, modelo.getCorreoElectronico());
+            ps.setString(12,modelo.getDireccionVivienda());
+            ps.setString(13, modelo.getMunicipio());
+            ps.setString(14, modelo.getAldeaColonia());
+            ps.setString(15, modelo.getDireccionVivienda());
+            ps.setString(16, modelo.getPuesto());
+            ps.setString(17, modelo.getTipoJornada());
+            ps.setString(18, modelo.getDiaInicioJornada());
+            ps.setString(19, modelo.getDiaFinJornada());
+            ps.setString(20, modelo.getHuella1());
+            ps.setString(21, modelo.getHuella2());
+            ps.setString(22, modelo.getHuella3());
+            ps.setString(23, modelo.getHuella4());
+
+
+            // Ejecutar y procesar resultado...
+        } catch (SQLException ex) {
+            conector.mensaje("No se pudo ingresar el empleado", "Error al eliminar", 0);
+            return resultado;
+        }
 
 
         return false;
@@ -35,7 +69,7 @@ public class EmpleadoImp implements IEmpleados {
             return ps.execute();
 
         } catch (SQLException ex) {
-           conector.mensaje("No se pudo eliminar el cliente", "Error al eliminar", 0);
+           conector.mensaje("No se pudo eliminar el empleado", "Error al eliminar", 0);
         return resultado;
         }
 
@@ -53,7 +87,47 @@ public class EmpleadoImp implements IEmpleados {
 
     @Override
     public ModeloEmpleado mostrarEmpleado(int dpi_empleado) {
-        return null;
+        boolean resultado = true;
+        conector.conectar();
+        ModeloEmpleado modelo = new ModeloEmpleado();
+        try{
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                ps =    conector.preparar(sql.getCONSULTA_TODOS_EMPLEADO());
+                ps.setInt(1, dpi_empleado);
+                modelo.setId_empleado(Integer.parseInt(rs.getString(1)));
+                modelo.setPrimerNombre(rs.getString(2));
+                modelo.setSegundoNombre(rs.getString(3));
+                modelo.setPrimerApellido(rs.getString(4));
+                modelo.setSegundoApellido(rs.getString(5));
+                modelo.setFechaNacimiento(rs.getString(6));
+                modelo.setEdad(Integer.parseInt(rs.getString(7)));
+                modelo.setDpi(rs.getString(8));
+                modelo.setNumeroTelefono(rs.getString(10));
+                modelo.setCorreoElectronico(rs.getString(11));
+                modelo.setDireccionVivienda(rs.getString(12));
+                modelo.setMunicipio(rs.getString(13));
+                modelo.setAldeaColonia(rs.getString(14));
+                modelo.setDireccionVivienda(rs.getString(15));
+                modelo.setPuesto(rs.getString(16));
+                modelo.setTipoJornada(rs.getString(17));
+                modelo.setDiaInicioJornada(rs.getString(18));
+                modelo.setDiaFinJornada(rs.getString(19));
+                modelo.setHuella1(rs.getString(20));
+                modelo.setHuella2(rs.getString(21));
+                modelo.setHuella3(rs.getString(22));
+                modelo.setHuella4(rs.getString(23));
+
+            }
+           conector.desconectar();
+
+
+        } catch (SQLException ex) {
+            conector.mensaje(ex.getMessage(), "Error al mostrar", 0);
+            conector.desconectar();
+        }
+        return modelo;
+
     }
 
     @Override
@@ -62,3 +136,4 @@ public class EmpleadoImp implements IEmpleados {
     }
 }
 //NITIDOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+//gerson te odio vv ojala don diablo ame mas aaxel
