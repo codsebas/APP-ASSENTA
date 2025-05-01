bef42ee9e7c05f4f1bb622fdc6d5b504cd155203
GersonGio <gorellanav1@miumg.edu.gt>
2025-04-30 21:16:04 -0600

    Agregue getter and setter faltante en modeloEmpleado

--- src/main/java/com/umg/modelos/ModeloEmpleado.java
+++ src/main/java/com/umg/modelos/ModeloEmpleado.java
@@ -92,6 +92,214 @@ public class ModeloEmpleado {
     public ModeloEmpleado(byte[] huella) {
         this.huella = huella;
     }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getDpi() {
+        return dpi;
+    }
+
+    public void setDpi(String dpi) {
+        this.dpi = dpi;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    public void setSegundoNombre(String segundoNombre) {
+        this.segundoNombre = segundoNombre;
+    }
+
+    public String getTercerNombre() {
+        return tercerNombre;
+    }
+
+    public void setTercerNombre(String tercerNombre) {
+        this.tercerNombre = tercerNombre;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getApellidoCasada() {
+        return apellidoCasada;
+    }
+
+    public void setApellidoCasada(String apellidoCasada) {
+        this.apellidoCasada = apellidoCasada;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getNumeroTelefono1() {
+        return numeroTelefono1;
+    }
+
+    public void setNumeroTelefono1(String numeroTelefono1) {
+        this.numeroTelefono1 = numeroTelefono1;
+    }
+
+    public String getNumeroTelefono2() {
+        return numeroTelefono2;
+    }
+
+    public void setNumeroTelefono2(String numeroTelefono2) {
+        this.numeroTelefono2 = numeroTelefono2;
+    }
+
+    public int getIdPuesto() {
+        return idPuesto;
+    }
+
+    public void setIdPuesto(int idPuesto) {
+        this.idPuesto = idPuesto;
+    }
+
+    public String getHorarioEntrada() {
+        return horarioEntrada;
+    }
+
+    public void setHorarioEntrada(String horarioEntrada) {
+        this.horarioEntrada = horarioEntrada;
+    }
+
+    public String getHorarioSalida() {
+        return horarioSalida;
+    }
+
+    public void setHorarioSalida(String horarioSalida) {
+        this.horarioSalida = horarioSalida;
+    }
+
+    public int getIdJefeInmediato() {
+        return idJefeInmediato;
+    }
+
+    public void setIdJefeInmediato(int idJefeInmediato) {
+        this.idJefeInmediato = idJefeInmediato;
+    }
+
+    public int getIdDireccion() {
+        return idDireccion;
+    }
+
+    public void setIdDireccion(int idDireccion) {
+        this.idDireccion = idDireccion;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getAldeaColonia() {
+        return aldeaColonia;
+    }
+
+    public void setAldeaColonia(String aldeaColonia) {
+        this.aldeaColonia = aldeaColonia;
+    }
+
+    public String getDireccionVivienda() {
+        return direccionVivienda;
+    }
+
+    public void setDireccionVivienda(String direccionVivienda) {
+        this.direccionVivienda = direccionVivienda;
+    }
+
+    public int getIdHuella() {
+        return idHuella;
+    }
+
+    public void setIdHuella(int idHuella) {
+        this.idHuella = idHuella;
+    }
+
+    public byte[] getHuella() {
+        return huella;
+    }
+
+    public void setHuella(byte[] huella) {
+        this.huella = huella;
+    }
 }
 
 
