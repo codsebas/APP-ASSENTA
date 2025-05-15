1e7164aaba7e79d81819857d09e60ded11e34a79
orem6 <korellanam6@miumg.edu.gt>
2025-05-14 22:51:21 -0600

    Se agrega vista Reporte Empleados

--- src/main/java/com/umg/vistas/VistaReporteInasistencia.java
+++ src/main/java/com/umg/vistas/VistaReporteInasistencia.java
@@ -155,10 +155,9 @@ public class VistaReporteInasistencia extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addContainerGap(10, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
